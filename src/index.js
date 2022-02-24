619250dded0d2b6d0a6d81aa7e4f61b8a6070546
IgorTargino <igortargino01@gmail.com>
2022-02-23 23:17:08 -0300

    feat: add route post withdraw

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ function verifyIfExistsAccountCpf(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -61,6 +73,27 @@ app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
     description,
     amount,
     type: "credit",
+    created_at: new Date()
+  }
+
+  customer.statement.push(customerOperation);
+
+  response.status(201).send();
+})
+
+app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    response.status(400).json({ error: "Insufficient funds!" })
+  }
+
+  const customerOperation = {
+    amount,
+    type: "debit",
     created_at: new Date()
   }
 
